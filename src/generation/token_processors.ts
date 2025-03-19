5c92ea62c1dd56389e36a4cb889de118acf21ebb
NzSN <nzsn0101@gmail.com>
2025-03-19 17:55:17 +0800

    Add TokenProcessor

---
+++ src/generation/token_processors.ts
@@ -0,0 +1,9 @@
+import { Token, TokenOperator } from './represent';
+
+class ModuleQualifier implements TokenOperator<void> {
+    public readonly ident = "ModuleQualifier";
+
+    public eval(t: Token): Token {
+
+    }
+}
